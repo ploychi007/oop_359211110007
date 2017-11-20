333fa6f356586eacb805431c5155fca7659b8108
ploychi007 <chawisaratpiboon@gmail.com>
2017-11-20 16:01:16 +0700

    Variable and Data type

---
+++ src/ooplab2/TestVariable.java
@@ -0,0 +1,48 @@
+package ooplab2;
+
+public class TestVariable {
+    public static void main(String[] args) {
+        //Integer
+        int i;
+        i = 10;
+        System.out.println("i = " + i);
+        int j = 20;
+        System.out.println("j = " + j);
+        System.out.println("i+j = "+(i+j));
+        //Double
+        double v;
+        v = 5.5;
+        System.out.println("v = " + v);
+        i = (int) v;
+        System.out.println("i = " + i);
+        //char
+        char c = 'x';
+        System.out.println("c = " + c);
+        c = 'y';
+        //String
+        String s;
+        String s1 = "Hello world";
+        System.out.println(s1 + "I'm PLOY");
+        int x = s1.length();
+        System.out.println("x = " + x);
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.concat("Chawisa"));
+        //String to int
+        String number = "50";
+        int y = Integer.parseInt(number);
+        System.out.println("y =" + y);
+        //Boolean
+        boolean b = true;
+        System.out.println("b = " + b);
+        b = false;
+        System.out.println("b = " + b);
+        //short cut operator
+        int z = 10;
+        int t = ++z;
+        System.out.println(z);
+        System.out.println(t);
+
+    }//main
+
+}//class
---
+++ src/ooplab2/TestinputData.java
@@ -0,0 +1,25 @@
+package ooplab2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class TestinputData {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("What is your name? :");
+        String name = reader.readLine();
+        System.out.println("Your name is name" + name);
+        System.out.print("How old are you?: ");
+        int age = Integer.parseInt(reader.readLine());
+        System.out.println("You are " + age + " years old.");
+        //Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Where are you?: ");
+        String address = scanner.nextLine();
+        System.out.println("Tou form: " + address);
+    }//main
+}//class
