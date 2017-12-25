103e8dbd7615e4ea3d92f2cf64261084727286b4
ploychi007 <chawisaratpiboon@gmail.com>
2017-12-25 18:15:14 +0700

    myStrBuffer

--- src/ooplab6/myStrBuffer.java
+++ src/ooplab6/myStrBuffer.java
@@ -2,6 +2,20 @@ package ooplab6;
 
 public class myStrBuffer {
     public static void main(String[] args) {
+// StringBuffer
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Hello");
+        buffer.append(" ");
+        buffer.append("t0");
+        buffer.append(" ");
+        buffer.append("RMUTSV");
+        buffer.append(" ");
+        buffer.append(2017);
+        System.out.println(buffer);
+        buffer.replace(0,5,"UniverSity");
+        System.out.println(buffer);
+        System.out.println(buffer.reverse());
+        buffer.setCharAt( 0,'T');
 
     }//main
 }//class
