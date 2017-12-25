1fe5a6ecbc5388fd77697421b72594cb0a02af4c
ploychi007 <chawisaratpiboon@gmail.com>
2017-12-25 18:27:05 +0700

    myStrToken

--- src/ooplab6/myStrToken.java
+++ src/ooplab6/myStrToken.java
@@ -1,7 +1,21 @@
 package ooplab6;
 
+import com.sun.javafx.tk.DummyToolkit;
+
+import java.util.StringTokenizer;
+
 public class myStrToken {
     public static void main(String[] args) {
+        //StringToken
+        String msg = "Welcome to RMUTSV";
+        StringTokenizer myToken = new StringTokenizer(msg);
+        System.out.println(myToken.countTokens());
+
+        while (myToken.hasMoreElements()){
+            StringBuffer myBuff = new StringBuffer(
+                    myToken.nextToken());
+            System.out.print(myBuff.reverse()+" ");
+        }
 
     }//main
 }//class
