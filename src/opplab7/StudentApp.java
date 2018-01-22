849053ed6f160bd2a185adf62851eca31353286d
LABCOM_6 <chawisaratpiboon@gmail.com>
2018-01-22 14:47:22 +0700

    Object and class with input Data

--- src/opplab7/StudentApp.java
+++ src/opplab7/StudentApp.java
@@ -1,5 +1,7 @@
 package opplab7;
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
         //create object as Student
@@ -12,8 +14,29 @@ public class StudentApp {
         Student s2= new Student("222222222222", "Gril Saiyai",
                 "Maketing","MK221");
         showData(s1);
+        showData(s2);
+
+
+        Student s3 = new Student();
+        s3 = inputData(s3);
 
     }//main
+
+    private static Student inputData(Student s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter student info:");
+        System.out.print("Enter student ID: ");
+        s.setId(scanner.nextLine());
+        System.out.print("Enter student name: ");
+        s.setName(scanner.nextLine());
+        System.out.print("Enter student Major: ");
+        s.setMajor(scanner.nextLine());
+        System.out.print("Enter student Group: ");
+        s.setGroup(scanner.nextLine());
+
+        return  s;
+    }//showData
+
 
     private static void showData(Student s) {
         System.out.println("Student info: ");
